73bc8a66f99a43df72d40c35358c73d2006f9444
Harryjeffs <harry.jeffs2222@gmail.com>
2021-05-12 11:07:09 +1000

    custom cells and further testing for out of place indexes

--- Sandbox Tests/Views/DeleteBackwardsTableViewController.swift
+++ Sandbox Tests/Views/DeleteBackwardsTableViewController.swift
@@ -13,7 +13,7 @@ class DeleteBackwardsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.register(cellType: UITableViewCell.self)
+        self.tableView.register(cellType: CustomCell.self)
         self.title = "Delete Test"
         
         // Data generation
@@ -30,13 +30,16 @@ class DeleteBackwardsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableData.count
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: UITableViewCell.self)
+    
+    fileprivate func kehfbhwebfbhe(_ tableView: UITableView, _ indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: CustomCell.self)
         let data = tableData[indexPath.row]
-        cell.textLabel?.text = "Index - \(data.index) - row \(indexPath.row)"
-        cell.backgroundColor = data.color
+        cell.resetData(data: data, indexPath: indexPath)
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return kehfbhwebfbhe(tableView, indexPath)
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -45,27 +48,45 @@ class DeleteBackwardsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        tableView.beginUpdates()
-        var allIndexPaths = [IndexPath]()
-        for index in 0...indexPath.row {
-            allIndexPaths.append(.init(row: index, section: 0))
+        DispatchQueue.main.async {
+            tableView.deselectRow(at: indexPath, animated: true)
+            tableView.beginUpdates()
+            var allIndexPaths = [IndexPath]()
+            for index in 0...indexPath.row {
+                allIndexPaths.append(.init(row: index, section: 0))
+            }
+            self.tableData.removeSubrange(0...indexPath.row)
+            tableView.deleteRows(at: allIndexPaths, with: .fade)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                tableView.reloadSections(IndexSet(integer: 0), with: .none)
+            }
+            
+            tableView.endUpdates()
         }
-        tableData.removeSubrange(0...indexPath.row)
-        tableView.deleteRows(at: allIndexPaths, with: .fade)
-        tableView.endUpdates()
-//        tableView.reloadData()
-        
+//        tableView.visibleCells.forEach { cell in
+//            guard let cell = cell as? CustomCell,
+//                  let indexPath = tableView.indexPath(for: cell) else { return }
+//            let data = self.tableData[indexPath.row]
+//            kehfbhwebfbhe(tableView, indexPath)
+////            cell.resetData(data: data, indexPath: indexPath)
+//        }
     }
     // MARK: - View Data
-    private struct TableContents {
+    struct TableContents {
         var color: UIColor = .random()
         var index: Int
     }
     
     private func generateData() {
         (0...100).forEach { i in
-            tableData.append(.init(index: i))
+            tableData.append(.init(index: i + 100))
+        }
+    }
+    
+    class CustomCell: UITableViewCell {
+        func resetData(data: TableContents, indexPath: IndexPath) {
+            textLabel?.text = "Index - \(data.index) - row \(indexPath.row)"
+            backgroundColor = data.color
         }
     }
 }
@@ -86,3 +107,4 @@ extension UIColor {
         )
     }
 }
+
